7661e8a83c944d2c29593897c2f873cd23bcb190
Robyn Choi <robyn3choi@gmail.com>
2024-02-26 16:10:59 -0800

    finished all functionality for beta

--- index.ts
+++ index.ts
@@ -23,7 +23,9 @@ const io = new Server(server, {
   },
 })
 
-const users: { name: string; room: string; isGm: boolean; socketId: string }[] = []
+type User = { id: string; name: string; room: string; isGm: boolean; socketId: string }
+
+const users: User[] = []
 
 app.get('/', (req, res) => {
   res.send('hello world')
@@ -32,29 +34,63 @@ app.get('/', (req, res) => {
 io.on('connection', (socket) => {
   console.log('connected', socket.id)
 
-  socket.on('userJoined', ({ room, username, isGm }) => {
+  socket.on('userJoined', ({ room, user }) => {
     socket.join(room)
 
     // existingUser should only be truthy during local dev
-    const existingUser = users.find((user) => user.name === username)
+    const existingUser = users.find((u) => u.id === user.id)
     if (existingUser) {
       existingUser.socketId === socket.id
     } else {
-      users.push({ name: username, room, isGm, socketId: socket.id })
+      users.push({ ...user, room, socketId: socket.id })
     }
 
-    console.log(`${isGm ? 'GM' : 'player'}: ${username} joined room: ${room}`)
+    console.log(`${user.isGm ? 'GM' : 'player'}: ${user.name} joined room: ${room}`)
 
     const usersInRoom = users.filter((p) => p.room === room)
 
     io.to(room).emit('usersUpdated', {
       updatedUsers: usersInRoom,
-      toastText: `${username} has joined the room as ${isGm ? 'the GM' : 'a player'}.`,
+      toastText: `${user.name} has joined the room as ${
+        user.isGm ? 'the GM. To invite other players, send them this page’s URL' : 'a player'
+      }.`,
+      isToastInfinite: true,
     })
   })
 
-  socket.on('passInitialDicePoolsAndCandles', ({ room, dicePools, candles }) => {
-    io.sockets.in(room).emit('passedInitialDicePoolsAndCandles', { dicePools, candles })
+  socket.on('passInitialState', ({ room, dicePools, candles, areCardsLocked }) => {
+    io.sockets.in(room).emit('passedInitialState', { dicePools, candles, areCardsLocked })
+  })
+
+  socket.on('updateGm', ({ room, oldGm, newGm }: { room: string; oldGm: User; newGm: User }) => {
+    let errorMsg
+
+    const _oldGm = users.find((u) => u.id === oldGm.id)
+    if (!_oldGm) {
+      errorMsg = `User with name: ${oldGm.name} and id: ${oldGm.id} not found`
+    }
+    const _newGm = users.find((u) => u.id === newGm.id)
+    if (!_newGm) {
+      errorMsg = `User with name: ${newGm.name} and id: ${newGm.id} not found`
+    }
+    if (errorMsg) {
+      console.error(errorMsg)
+      return io
+        .to(room)
+        .emit('error', { message: `There was a problem reassigning the GM. Error message - ${errorMsg}` })
+    }
+
+    _oldGm!.isGm = false
+    _newGm!.isGm = true
+
+    console.log(`${_newGm!.name} is now the GM in room: ${room}`)
+
+    const usersInRoom = users.filter((p) => p.room === room)
+
+    io.to(room).emit('usersUpdated', {
+      updatedUsers: usersInRoom,
+      toastText: `${_newGm!.name} is now the GM.`,
+    })
   })
 
   socket.on('candleChange', ({ room, username, index, isLit }) => {
@@ -72,12 +108,28 @@ io.on('connection', (socket) => {
     io.sockets.in(room).emit('rolled', { dicePool, dice, username })
   })
 
-  socket.on('dragStart', ({ username, room, dieId, dicePool }) => {
-    io.sockets.in(room).emit('dragStarted', { dicePool, username, dieId })
+  socket.on('dieDragStart', ({ username, room, dieId, dicePool }) => {
+    io.sockets.in(room).emit('dieDragStarted', { dicePool, username, dieId })
   })
 
-  socket.on('dragEnd', ({ username, room, dieId, prevDicePool, newDicePool }) => {
-    io.sockets.in(room).emit('dragEnded', { prevDicePool, newDicePool, username, dieId })
+  socket.on('dieDragEnd', ({ username, room, dieId, prevDicePool, newDicePool }) => {
+    io.sockets.in(room).emit('dieDragEnded', { prevDicePool, newDicePool, username, dieId })
+  })
+
+  socket.on('transferCard', ({ newUsername, oldUsername, room, card }) => {
+    io.sockets.in(room).emit('cardTransferred', { newUsername, oldUsername, card })
+  })
+
+  socket.on('changeLock', ({ isLocked, room }) => {
+    io.sockets.in(room).emit('lockChanged', isLocked)
+  })
+
+  socket.on('updatePeerUserCards', ({ room, userId, cards, toastText }) => {
+    io.sockets.in(room).emit('peerUserCardsUpdated', { userId, cards, toastText })
+  })
+
+  socket.on('revealBrink', ({ room, userId }) => {
+    io.sockets.in(room).emit('brinkRevealed', { userId })
   })
 
   socket.on('disconnecting', (reason) => {
@@ -87,11 +139,20 @@ io.on('connection', (socket) => {
     })
 
     let removedUser
+    let newGm
 
     const index = users.findIndex((u) => u.socketId === socket.id)
     if (index > -1) {
       ;[removedUser] = users.splice(index, 1)
-      console.log(`Player: ${removedUser.name} with socketId: ${socket.id} left room: ${room}`)
+      console.log(`User: ${removedUser.name} with socketId: ${socket.id} left room: ${room}`)
+
+      if (removedUser.isGm) {
+        newGm = users.find((u) => u.room === room)
+        if (newGm) {
+          newGm.isGm = true
+          console.log(`User: ${newGm.name} is now the GM in room: ${room}`)
+        }
+      }
     } else {
       console.log(
         `Tried to remove user with socketId: ${socket.id} from room: ${room}, but it didn't exist in the users array`
@@ -101,19 +162,16 @@ io.on('connection', (socket) => {
 
     io.to(room).emit('usersUpdated', {
       updatedUsers: usersInRoom,
-      toastText: removedUser ? `${removedUser.name} has left the room.` : null,
+      toastText: removedUser
+        ? `${removedUser.name} has left the room.${newGm ? ` ${newGm.name} is now the GM.` : ''}`
+        : null,
     })
   })
 })
 
-app.get('/user', (req, res) => {
-  const { username, room } = req.query
-  res.send({ doesUserExist: !!users.find((u) => u.room === room && u.name === username) })
-})
-
-app.get('/room', (req, res) => {
+app.get('/users', (req, res) => {
   const { room } = req.query
-  res.send({ doesRoomExist: !!users.find((u) => u.room === room) })
+  res.send(users.filter((u) => u.room === room))
 })
 
 server.listen(port, () => {
